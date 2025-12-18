8e16b374ea4c3d7de9a8dbfe85d0ab7050ebee83
Jonathan Soto <jonasotoaguilar@gmail.com>
2025-12-18 20:35:44 -0300

    fix(layout): add suppressHydrationWarning to html tag to fix hydration mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className="dark">
+    <html lang="es" className="dark" suppressHydrationWarning>
       <body
         className={cn(
           inter.className,
