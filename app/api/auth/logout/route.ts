1a4421d8c5c9bd5960679e74783f8bcfc06be546
Jonathan Soto <jonasotoaguilar@gmail.com>
2026-01-08 13:12:20 -0300

    fix(auth): redirect to login after logout

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: Request) {
   const appUrl = new URL(request.url).origin;
 
   const logoutUrl = `/outpost.goauthentik.io/auth/logout?rd=${encodeURIComponent(
-    appUrl
+    appUrl + "/login"
   )}`;
 
   return NextResponse.redirect(logoutUrl);
